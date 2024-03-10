c2024a34a6706f0f96a078705dcc2bb4fd1854b9
Nick <nick@nowplus.org>
2024-03-10 09:12:09 +0800

    Removed api context so that api instance can be used in server actions

--- client-app/app/_hooks/useApi.js
+++ client-app/app/_hooks/useApi.js
@@ -1,7 +1,57 @@
-import { useApiContext } from '@/contexts/ApiContext';
+import axios from "axios";
+import { useToken } from "./useToken";
+import { useMemo } from "react";
 
 export const useApi = () => {
-  const api = useApiContext();
+  const { getToken, setToken } = useToken();
+
+  const api = useMemo(() => {
+    const instance = axios.create({
+      baseURL: process.env.NEXT_PUBLIC_BASE_URL
+    });
+
+    // Request interceptor
+    instance.interceptors.request.use((config) => {
+      const token = getToken();
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    }, (error) => Promise.reject(error));
+
+    // Response interceptor
+    instance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+    
+        if (error.response.status === 401 && !originalRequest._retry) {
+          originalRequest._retry = true;
+          try {
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/renew-token`, {
+              withCredentials: true // Attach refresh-token
+            });
+    
+            // Overwrite old token with new one
+            const newToken = response.data.accessToken;
+            setToken(newToken);
+    
+            originalRequest.headers.Authorization = `Bearer ${newToken}`;
+            return axios(originalRequest); // LINE 45
+    
+          } catch (error) {
+            // redirect to login (unless page is already login)
+            if (typeof window !== "undefined" && window.location.pathname !== '/login') {
+              // Perform a client-side redirect
+              window.location.href = '/login';
+            }
+          }
+        }
+        return Promise.reject(error);
+      });
+
+    return instance;
+  }, [getToken, setToken]);
 
   return { api };
 };
--- client-app/app/providers.js
+++ client-app/app/providers.js
@@ -3,18 +3,15 @@
 import { ItemsProvider } from "@/contexts/ItemsContext";
 import { ElementRefsProvider } from "@/contexts/ElementRefsContext";
 import { MessagesProvider } from "@/contexts/MessagesContext";
-import { ApiProvider } from "@/contexts/ApiContext";
 
 export function Providers({ children }) {
   return (
-    <ApiProvider>
-      <MessagesProvider>
-        <ElementRefsProvider>
-          <ItemsProvider>
-            {children}
-          </ItemsProvider>
-        </ElementRefsProvider>
-      </MessagesProvider>
-    </ApiProvider>
+    <MessagesProvider>
+      <ElementRefsProvider>
+        <ItemsProvider>
+          {children}
+        </ItemsProvider>
+      </ElementRefsProvider>
+    </MessagesProvider>
   );
 }
